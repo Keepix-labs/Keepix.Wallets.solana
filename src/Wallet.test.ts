547e4ff3a9a6df70cf27ba67cd00884058a75c85
jode-vri <jode-vri@student.42.fr>
2023-12-10 17:28:36 +0100

    Add Wallet class and tests

---
+++ src/Wallet.test.ts
@@ -0,0 +1,106 @@
+import { generateMnemonic, mnemonicToSeedSync } from 'bip39';
+import { Keypair } from '@solana/web3.js';
+import { Wallet } from './Wallet';
+
+
+let mockedValues = {};
+let queryMockedCallBack = (url) => {
+
+  if (mockedValues[url]) {
+    return mockedValues[url];
+  } else {
+    console.log('mock not intercepted', url);
+  }
+  return new Promise(resolve => resolve({
+    json: () => {
+      return new Promise(res => res(''));
+    }
+  }));
+};
+
+jest.mock('node-fetch', () => {
+  const originalModule = jest.requireActual('node-fetch');
+
+  //Mock the default export and named export 'foo'
+  return {
+    __esModule: true,
+    ...originalModule,
+    default: jest.fn((url) => {
+        return queryMockedCallBack(url);
+      }
+    )
+  };
+});
+
+const mockFetch = () => {
+  
+}
+
+describe('basic wallet', () => {
+    const mnemonic = 'cross tool vague fresh fever bind permit round arrest train reason sentence math comfort castle amount velvet elevator split loyal foil bundle elite museum'
+    const address = 'DehxSQMZoTSKUXg9utFFemvuY7N37kkMh9FnJCGrtDHs'
+    const privateKey = '0ceab9e154b198dec48b4d23028992e1c29d9b8ec989d24576b3e367e284e7d2bbf50abd75fad249c4d8225ff7c9708be0c705e010a7cb0878f103dcf2bfec2a'
+
+    it('can generate with random', async () => {
+        const wallet = new Wallet({  });
+    
+        expect(wallet.getAddress()).toBeDefined();
+        expect(wallet.getPrivateKey).toBeDefined();
+        expect(wallet.getMnemonic()).toBeDefined();
+    }, 60000);
+
+    it('can generate same wallet', async () => {
+        const wallet = new Wallet({ password: 'toor',  });
+
+        expect(wallet.getAddress()).toEqual(address);
+        expect(wallet.getPrivateKey()).toEqual(privateKey);
+        expect(wallet.getMnemonic()).toEqual(mnemonic);
+    }, 60000);
+
+    it('can generate with PrivateKey', async () => {
+        const wallet = new Wallet({ privateKey: privateKey,  });
+
+        expect(wallet.getAddress()).toEqual(address);
+        expect(wallet.getPrivateKey()).toEqual(privateKey);
+    }, 60000);
+
+    it('can generate with Mnemonic', async () => {
+        const seed = mnemonicToSeedSync(mnemonic, "");
+
+        const keypair = Keypair.fromSeed(seed.slice(0, 32));
+        const wallet = new Wallet({ mnemonic: mnemonic });
+
+        expect(wallet.getAddress()).toEqual(keypair.publicKey.toString());
+        expect(wallet.getPrivateKey()).toEqual(Buffer.from(keypair.secretKey).toString('hex'));
+        expect(wallet.getMnemonic()).toEqual(mnemonic);
+    }, 60000);
+
+    it('can get balance', async () => {
+        const wallet = new Wallet({ password: 'toor' });
+
+        expect(await wallet.getCoinBalance()).toEqual(0);
+    }, 60000);
+
+    it('can get token balance', async () => {
+        const wallet = new Wallet({ password: 'toor' });
+
+        expect(await wallet.getTokenBalance('DezXAZ8z7PnrnRJjz3wXBoRgixCa6xjnB7YaB1pPB263', address)).toEqual(0);
+    }, 60000);
+
+    it('can estimate sendCoin', async () => {
+      const wallet = new Wallet({ password: 'toor' });
+      const estimationResult = await wallet.estimateCostSendCoinTo('Cd5PHnve4rJDHBAGvXgJnouddALyX3NmbwN3s3MzvPBQ', 1);
+
+      expect(estimationResult?.success).toBe(false);
+      expect(estimationResult?.description).toMatch('insufficient funds');
+    });
+
+    it('can estimate sendToken', async () => {
+      const wallet = new Wallet({ password: 'toor' });
+      const estimationResult = await wallet.estimateCostSendTokenTo('DezXAZ8z7PnrnRJjz3wXBoRgixCa6xjnB7YaB1pPB263', 'Cd5PHnve4rJDHBAGvXgJnouddALyX3NmbwN3s3MzvPBQ', 1);
+
+      console.log(estimationResult?.description);
+      expect(estimationResult?.success).toBe(false);
+      expect(estimationResult?.description).toMatch('insufficient funds');
+    });
+})
---
+++ src/Wallet.ts
@@ -0,0 +1,205 @@
+import { mnemonicToSeedSync, entropyToMnemonic } from 'bip39';
+import { 
+    Keypair,
+    Connection,
+    clusterApiUrl,
+    PublicKey,
+    Transaction,
+    SystemProgram,
+    LAMPORTS_PER_SOL
+} from '@solana/web3.js';
+import {
+    createTransferCheckedInstruction,
+} from "@solana/spl-token";
+import { randomBytes } from 'crypto';
+
+function createPrivateKey(templatePrivateKey: string, password: string) {
+    const crypto = require('crypto');
+    const hash = crypto.createHash('sha256').update(templatePrivateKey + password, 'utf8').digest('hex');
+    return hash.substring(0, 64);
+}
+
+export class Wallet {
+
+    private keypair: Keypair;
+    private rpc: Connection;
+    private mnemonic?: string;
+    private keepixTokens?: { coins: any, tokens: any };
+
+    constructor({
+        password,
+        mnemonic,
+        privateKey,
+        keepixTokens,
+        privateKeyTemplate = '0x2050939757b6d498bb0407e001f0cb6db05c991b3c6f7d8e362f9d27c70128b9'
+    }:{
+        password?: string,
+        mnemonic?: string,
+        privateKey?: string,
+        keepixTokens?: { coins: any, tokens: any }
+        privateKeyTemplate?: string
+    }) {
+        this.keypair = Keypair.generate();
+        this.keepixTokens = keepixTokens;
+        this.rpc = new Connection(clusterApiUrl("mainnet-beta"), "confirmed");
+
+        // from password
+        if (password !== undefined) {
+            const newPrivateKeySol = createPrivateKey(privateKeyTemplate, password);
+            this.mnemonic = entropyToMnemonic(Buffer.from(newPrivateKeySol, 'hex'));
+            const seed = mnemonicToSeedSync(this.mnemonic, "");
+            this.keypair = Keypair.fromSeed(seed.slice(0, 32));
+            return ;
+        }
+        // from mnemonic
+        if (mnemonic !== undefined) {
+            this.mnemonic = mnemonic;
+            const seed = mnemonicToSeedSync(mnemonic, "");
+            this.keypair = Keypair.fromSeed(seed.slice(0, 32));
+            return ;
+        }
+        // from privateKey only
+        if (privateKey !== undefined) {
+            this.mnemonic = undefined;
+            this.keypair = Keypair.fromSecretKey(Buffer.from(privateKey, 'hex'));
+            return ;
+        }
+        this.mnemonic = entropyToMnemonic(randomBytes(32));
+        this.keypair = Keypair.generate();
+    }
+
+    // PUBLIC
+
+    public getPrivateKey() {
+        return Buffer.from(this.keypair.secretKey).toString('hex');
+    }
+
+    public getMnemonic() {
+        return this.mnemonic;
+    }
+
+    public getAddress() {
+        return this.keypair.publicKey.toString();
+    }
+
+    public getProdiver() {
+        return this.rpc.rpcEndpoint;
+    }
+
+    public async getCoinBalance(walletAddress?: string) {
+        let publicKey;
+
+        if (walletAddress)
+            publicKey = new PublicKey(walletAddress);
+        else
+            publicKey = new PublicKey(this.keypair.publicKey.toString())
+        return (await this.rpc.getBalance(publicKey) / LAMPORTS_PER_SOL);
+    }
+
+    public async getTokenBalance(tokenAddress: string, walletAddress?: string) {
+        let publicKey;
+
+        if (walletAddress) {
+            publicKey = new PublicKey(walletAddress);
+        } else {
+            publicKey = this.keypair.publicKey;
+        }
+
+        const tokenAccounts = await this.rpc.getParsedTokenAccountsByOwner(publicKey, { mint: new PublicKey(tokenAddress) });
+        let tokenBalance: number = 0;
+
+        for (const accountInfo of tokenAccounts.value) {
+            const decimals = accountInfo.account.data["parsed"]["info"]["tokenAmount"]["decimals"];
+            const amount = accountInfo.account.data["parsed"]["info"]["tokenAmount"]["amount"];
+
+            tokenBalance = Number((amount / Math.pow(10, decimals)).toFixed(decimals));
+        }
+
+        return tokenBalance;
+    }
+
+    public async estimateCostOfTx(tx: any) {
+        try {
+            const recentBlockhash = await this.rpc.getRecentBlockhash();
+
+            tx.recentBlockhash = recentBlockhash.blockhash;
+            tx.sign(this.keypair);
+
+            const serializedTx = tx.serialize();
+            const txSize = serializedTx.length;
+            const feeRate = recentBlockhash.feeCalculator.lamportsPerSignature;
+            const estimatedCost = txSize * feeRate;
+
+            return { success: true, description: `${estimatedCost}` };
+        } catch (error) {
+            return { success: false, description: `Getting estimation failed: ${error}` };
+        }
+    }
+
+    public async estimateCostSendCoinTo(receiverAddress: string, amount: number) {
+        try {
+            const receiverPublicKey = new PublicKey(receiverAddress);
+            const walletBalance = await this.getCoinBalance();
+
+            if (walletBalance < amount) {
+                return { success: false, description: `insufficient funds` };
+            }
+
+            const transaction = new Transaction();
+
+            transaction.add(
+                SystemProgram.transfer({
+                    fromPubkey: this.keypair.publicKey,
+                    toPubkey: receiverPublicKey,
+                    lamports: amount * 1e9,
+                })
+            );
+            const estimatedCost = await this.estimateCostOfTx(transaction);
+
+            return estimatedCost;
+        } catch (error) {
+            return { success: false, description: `Getting estimation failed: ${error}` };
+        }
+    }
+
+    public async estimateCostSendTokenTo(tokenAddress: string, receiverAddress: string, amount: number) {
+        try {
+            const receiverPublicKey = new PublicKey(receiverAddress);
+            const tokenBalance = await this.getTokenBalance(tokenAddress);
+
+            if (tokenBalance < Number(amount)) {
+                return { success: false, description: 'insufficient funds' };
+            }
+
+            // const test = new PublicKey('DezXAZ8z7PnrnRJjz3wXBoRgixCa6xjnB7YaB1pPB263');
+            const tokenAccount = await this.rpc.getParsedTokenAccountsByOwner(this.keypair.publicKey, { mint: new PublicKey(tokenAddress) });
+
+            const transaction = new Transaction();
+
+            transaction.add(
+                createTransferCheckedInstruction(
+                    this.keypair.publicKey,
+                    new PublicKey(tokenAddress),
+                    receiverPublicKey,
+                    this.keypair.publicKey,
+                    amount * 1e9,
+                    tokenAccount.value[0].account.data.parsed.info.decimals,
+                )
+            );
+
+            const estimatedCost = await this.estimateCostOfTx(transaction);
+
+            return estimatedCost;
+        } catch (error) {
+            return { success: false, description: `Getting estimation failed: ${error}` };
+        }
+    }
+
+    public async sendCoinTo(receiverAddress: string, amount: string) {
+
+    }
+
+    public async sendTokenTo(tokenAddress: string, receiverAddress: string, amount: string) {
+
+    }
+}
